2aac50e120f0887d3b781e1603edbcac808e87d8
Adriano Abner Da Silva Bezerra <adriano.abner@live.com>
2026-07-28 15:05:40 -0300

    fix: norminette

--- tests/main_test.c
+++ tests/main_test.c
@@ -6,7 +6,7 @@
 /*   By: adrianda <adrianda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/22 17:59:37 by adrianda          #+#    #+#             */
-/*   Updated: 2026/07/27 17:34:07 by adrianda         ###   ########.fr       */
+/*   Updated: 2026/07/28 15:05:18 by adrianda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,14 @@ static void	test_one(char *op_name, void (*op)(t_push_swap *))
 	int			values_b[0];
 	t_push_swap	*push_swap;
 
-
 	push_swap = build_dummy_stack(values_a, 0, values_b, 0);
-	
 	printf("=== %s | ANTES ===\n", op_name);
 	print_both(push_swap);
 	op(push_swap);
 	printf("=== %s | DEPOIS ===\n", op_name);
 	print_both(push_swap);
 	printf("\n");
-	printf("%d",push_swap->op_counts);
+	printf("%d", push_swap->op_counts);
 	free_stack(push_swap->stack_a);
 	free_stack(push_swap->stack_b);
 	free(push_swap);
@@ -44,10 +42,8 @@ static void	test_one(char *op_name, void (*op)(t_push_swap *))
 int	main(void)
 {
 	test_one("sb", sa);
-
 	return (0);
 }
-
 /*
 	test_one("sa", sa);
 	test_one("sb", sb);
