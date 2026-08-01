939ed2b175df8c4114b28e415408b645f7335e64
Adriano Abner Da Silva Bezerra <adriano.abner@live.com>
2026-08-01 17:23:23 -0300

    Removing 'tail', creating a 'find tail' function, and organizing the .h file.

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: adrianda <adrianda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/16 13:08:00 by adrianda          #+#    #+#             */
-/*   Updated: 2026/07/30 20:47:01 by adrianda         ###   ########.fr       */
+/*   Updated: 2026/08/01 17:00:24 by adrianda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,49 +50,43 @@ typedef enum e_op
 typedef struct s_push_swap
 {
 	t_node	*stack_a;
-	t_node	*tail_a;
 	t_node	*stack_b;
-	t_node	*tail_b;
 	int		op_counts[OP_TYPES_TOTAL];
 }	t_push_swap;
 
-int				is_valid(char *str);
-int				has_duplicate(t_push_swap *push_swap, int temp_num);
-// push
-void			pa(t_push_swap *push_swap);
-void			pb(t_push_swap *push_swap);
-// rotate reverse
-void			rra(t_push_swap *push_swap);
-void			rrb(t_push_swap *push_swap);
-void			rrr(t_push_swap *push_swap);
-// rotate
-void			ra(t_push_swap *push_swap);
-void			rb(t_push_swap *push_swap);
-void			rr(t_push_swap *push_swap);
-// swap
-void			sa(t_push_swap *push_swap);
-void			sb(t_push_swap *push_swap);
-void			ss(t_push_swap *push_swap);
-// stack
-int				stack_size(t_node *stack);
-int				is_empty(t_node *stack);
-t_node			*second_last(t_node *stack);
-int				stack_a_add_back(t_push_swap *push_swap, int value);
-int				is_sorted(t_node *stack);
-// node
-int				node_compare(t_node *src, t_node *dest);
-t_node			*get_node(t_node *stack, int type);
-// metrics
-void			count_op(t_push_swap *push_swap, t_op op);
-// SIMPLE -> controller
-void			simple_sort(t_push_swap *push_swap);
-
-// parser
-int	is_valid(char *str);
-int	has_duplicate(t_push_swap *push_swap, int temp_num);
-int	is_flag(char *str);
-int	is_int_range(char *str);
-
+/* --- PARSER --- */
+int		is_valid(char *str);
+int		has_duplicate(t_push_swap *push_swap, int temp_num);
+int		is_flag(char *str);
+int		is_int_range(char *str);
+/* --- PUSH --- */
+void	pa(t_push_swap *push_swap);
+void	pb(t_push_swap *push_swap);
+/* --- ROTATE REVERSE --- */
+void	rra(t_push_swap *push_swap);
+void	rrb(t_push_swap *push_swap);
+void	rrr(t_push_swap *push_swap);
+/* --- ROTATE --- */
+void	ra(t_push_swap *push_swap);
+void	rb(t_push_swap *push_swap);
+void	rr(t_push_swap *push_swap);
+/* --- SWAP --- */
+void	sa(t_push_swap *push_swap);
+void	sb(t_push_swap *push_swap);
+void	ss(t_push_swap *push_swap);
+/* --- STACK --- */
+int		stack_size(t_node *stack);
+int		is_empty(t_node *stack);
+t_node	*second_last(t_node *stack);
+int		stack_a_add_back(t_push_swap *push_swap, int value);
+int		is_sorted(t_node *stack);
+/* --- NODE --- */
+int		node_compare(t_node *src, t_node *dest);
+t_node	*get_node(t_node *stack, int type);
+/* --- METRICS --- */
+void	count_op(t_push_swap *push_swap, t_op op);
+/* --- SIMPLE CONTROLLER --- */
+void	simple_sort(t_push_swap *push_swap);
 // temporário, só para ambiente de testes
 t_push_swap		*build_dummy_stack(int *values_a, int n_a,
 					int *values_b, int n_b);
